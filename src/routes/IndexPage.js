1e3fe38b3bdf86e3f0077d0b23b8bbdd5d09cb85
lsyaaaaaaa <lsyaaaaaaa@163.com>
2020-12-21 11:50:21 +0800

    响应式1

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'dva';
-import { Layout, Drawer, Button, Badge } from 'antd';
-import {ShoppingCartOutlined } from '@ant-design/icons';
+import { Layout, Drawer, Button, Badge, Row, Col } from 'antd';
+import { ShoppingCartOutlined } from '@ant-design/icons';
 import styles from "./IndexPage.css"
 import Goods from '../components/Goods'
 import Cart from '../components/Cart';
@@ -36,40 +36,46 @@ class IndexPage extends React.Component {
 
 
   render() {
-    const { Content, Sider } = Layout;
-    const {count} = this.props
+    const { Content, Sider, Footer } = Layout;
+    const { count } = this.props
     return (
-      <div>
-        <Layout style={{backgroundColor:"white"}}>
-          <Sider theme="light" style={{  zIndex: 1, width: '100%', marginLeft:74, marginTop:84 }}>
-            <div className="logo" />
-            <Screen />
-          </Sider>
-          <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 , background:"white" }}>
-            <div className={styles.productlist} style={{ padding: 24, minHeight: 580, display: 'flex', flexFlow: 'row wrap', justifyContent: 'space-around' }}>
-              <Goods />
-            </div>
-          </Content>
-          <Sider theme="light" style={{ textAlign: 'right',fontSize:"30px" }}>
-            <div style={{ zIndex: 2,top: 16, left: "80%" }} >
-              <Badge count={count} showZero offset={[-9, 28]}  size="small" style={{backgroundColor:'#1890FF'}}>
-                <Button icon={<ShoppingCartOutlined />} style={{fontSize:"16px", backgroundColor:'black', border:'none'}} type="primary" size="large" shape="" onClick={this.showDrawer} >
-        
+      <div >
+        <Row >
+          <Layout style={{ background: 'white' }}>
+            <Col xs={6} xl={4}>
+              <Sider theme="light" style={{ zIndex: 1, width: '100%', marginLeft: 74, marginTop: 84 }}>
+                <div className="logo" />
+                <Screen />
+              </Sider>
+            </Col>
+            <Col xs={18} xl={20}>
+              <Layout style={{ background: 'white' }}>
+                <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64, background: "white" }}>
+                  <div className={styles.productlist} style={{ padding: 24, minHeight: 580, display: 'flex', flexFlow: 'row wrap', justifyContent: 'space-around', width: '100%' }}>
+                    <Goods />
+                  </div>
+                </Content>
+                <Footer style={{ textAlign: 'center', fontSize: "30px" }}></Footer>
+              </Layout>
+            </Col>
+            <div style={{ zIndex: 2, top: 16, left: "80%" }} >
+              <Badge count={count} showZero offset={[-9, 28]} size="small" style={{ backgroundColor: '#1890FF' }}>
+                <Button icon={<ShoppingCartOutlined />} style={{ fontSize: "16px", backgroundColor: 'black', border: 'none' }} type="primary" size="large" shape="" onClick={this.showDrawer} >
+
                 </Button>
               </Badge>
             </div>
-          </Sider>
-        </Layout>
-        
-        <Drawer
-          title='Cart'
-          width="550"
-          placement="right"
-          onClose={this.onClose}
-          visible={this.state.visible}
-        >
-          <Cart />
-        </Drawer>
+            <Drawer
+              title='Cart'
+              width="550"
+              placement="right"
+              onClose={this.onClose}
+              visible={this.state.visible}
+            >
+              <Cart />
+            </Drawer>
+          </Layout>
+        </Row>
       </div>
     );
   }
