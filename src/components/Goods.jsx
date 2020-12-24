c1ad5de4681a6732af3af08a6ca630326970c054
lsyaaaaaaa <lsyaaaaaaa@163.com>
2020-12-24 11:31:15 +0800

    响应式

--- src/components/Goods.jsx
+++ src/components/Goods.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'dva';
-import { Card, Button, Popover, List, Menu, Tooltip } from 'antd';
+import { Card, Button, Popover, List, Menu, Tooltip, notification, Row, Col } from 'antd';
 import { ProjectOutlined } from '@ant-design/icons';
 import {
     SyncOutlined,
@@ -18,7 +18,9 @@ class Goods extends React.Component {
     constructor() {
         super()
         this.state = {
-            key: 0
+            key: 0,
+            visible: [],
+            sizeKey: ''
         }
     }
 
@@ -40,7 +42,7 @@ class Goods extends React.Component {
         })
     }
 
-    addCart = async (data, size) => {
+    addCart = async (data, size, index, type) => {
         const { dispatch } = this.props
         await dispatch({
             type: 'cart/addCart',
@@ -49,6 +51,16 @@ class Goods extends React.Component {
                 size: size
             }
         })
+        let arr = this.state.visible
+        arr[index] = false
+        this.setState({
+            visible: arr,
+        });
+        notification[type]({
+            message: '添加成功',
+            description:
+                '',
+        });
     }
 
     onCollate = async (key) => {
@@ -63,34 +75,58 @@ class Goods extends React.Component {
         })
     }
 
+    handleVisibleChange = (e, index) => {
+        let arr = this.state.visible
+        arr[index] = e
+        this.setState({ visible: arr });
+    };
+
     render() {
         const { productData } = this.props;
         const { SubMenu } = Menu;
         // const { Option } = Select;
 
-        const list = (productData || []).map((item, key) => (
-            <Card className="cart" style={{ width: 300, margin: 10, borderRadius: "2%" }} key={key}>
-                <Tooltip placement="topLeft" title="Free Shopping" arrowPointAtCenter>
-                   <img src={`./img/${item.sku}_1.jpg`} alt={item.title + "_1.jpg"} style={{ width: 252 }}></img>
-                </Tooltip>
-                
-                <h3 style={{ textAlign: 'center' }}>{item.title}</h3>
-                <hr style={{ width: "10%", backgroundColor: 'black' }} />
-                <h2 style={{ textAlign: 'center' }}>{item.currencyFormat + item.price}</h2>
-                <Popover
-                    content={
-                        <List
-                            size="small"
-                            dataSource={item.availableSizes}
-                            renderItem={size => <List.Item><Button onClick={() => this.addCart(item, size)} block>{size}</Button></List.Item>}
-                        />
-                    }
-                    title="请选择需要的尺码"
-                    trigger="click">
-                    <Button style={{ backgroundColor: 'black', color: "#fff" }} size="large" block>Add to cart</Button>
-                </Popover>
-            </Card>
-        ));
+        const list = (productData || []).map((item, key) => {
+            let newPrice = item.price + ''
+            const newPrices = newPrice.split(".");
+            const num1 = newPrices[0] //整数
+            const num2 = newPrices[1] //小数
+            return (
+                <Card className="cart" hoverable style={{ width: 300, margin: 10, borderRadius: "2%" }} key={key}>
+                    <Tooltip placement="topLeft" title="Free Shopping" arrowPointAtCenter>
+                        <img src={`./img/${item.sku}_1.jpg`} alt={item.title + "_1.jpg"} style={{ width: 252 }}></img>
+                    </Tooltip>
+
+                    <h3 style={{ textAlign: 'center' }}>{item.title}</h3>
+                    <hr style={{ width: "10%", backgroundColor: 'black' }} />
+                    {/* <h2 style={{ textAlign: 'center' }}>{item.currencyFormat + item.price}</h2> */}
+
+                    <div style={{ textAlign: 'center' }}>
+                        {/* <h3>{item.currencyFormat + num1 + '.' + num2}</h3> */}
+                        <span style={{ fontSize: '20px' }}>{item.currencyFormat}</span>
+                        <span style={{ fontSize: '30px' }}>{num1}</span>
+                        <span>.</span>
+                        <span style={{ fontSize: '20px' }}>{num2 ? num2 : 0}</span>
+                    </div>
+
+                    <Popover
+                        content={
+                            <List
+                                size="small"
+                                dataSource={item.availableSizes}
+                                renderItem={size => <List.Item><Button onClick={() => this.addCart(item, size, key, 'success')} block>{size}</Button></List.Item>}
+                            />
+                        }
+                        title="请选择需要的尺码"
+                        visible={this.state.visible[key]}
+                        trigger="click"
+                        onVisibleChange={(e) => this.handleVisibleChange(e, key)}
+                    >
+                        <Button style={{ backgroundColor: 'black', color: "#fff" }} size="large" block>Add to cart</Button>
+                    </Popover>
+                </Card>
+            )
+        });
 
         // function handleChange(value) {
         //   console.log(`selected ${value}`);
@@ -98,36 +134,28 @@ class Goods extends React.Component {
 
         return (
             <>
-                <div style={{ width: "100%", display: "flex", justifyContent: "space-between" }}>
-                    <div>
-                        <h3>{` ${productData.length} Product(s) found`}</h3>
-                    </div>
-                    <div style={{ display: "flex", marginRight: '50px', alignItems: 'center' }}>
-                        <h3 style={{ fontWeight: 'bold' }} onChange={this.handleSizeChange}>Order by &nbsp;</h3>
-
-                        {/* <Select defaultValue="Comprehensive Ranking" style={{ width: 180 }} onChange={handleChange}>
-                            <Option value="Comprehensive Ranking" key="setting:1" onClick={() => this.onCollate()}>
-                                Comprehensive Ranking
-                            </Option>
-                            <Option value="Lowest to Highest" key="setting:2" onClick={() => this.onCollate('up')}>
-                                Lowest to Highest
-                            </Option>
-                            <Option value="Highest to Lowest" key="setting:3" onClick={() => this.onCollate('down')}>
-                                Highest to Lowest
-                            </Option>
-                        </Select> */}
-
-                        <span >
-                            <Menu theme="dark" style={{color:'white'}} mode="horizontal" defaultSelectedKeys={['1']} >
-                                <SubMenu icon={<ProjectOutlined />} className={GoodStyle.menu} title="Comprehensive Ranking">
-                                    <Menu.Item key="setting:1" onClick={() => this.onCollate()}>Comprehensive Ranking</Menu.Item>
-                                    <Menu.Item key="setting:2" onClick={() => this.onCollate('up')}>Lowest to Highest</Menu.Item>
-                                    <Menu.Item key="setting:3" onClick={() => this.onCollate('down')}>Highest to Lowest</Menu.Item>
-                                </SubMenu>
-                            </Menu>
-                        </span>
-                    </div>
-
+                <div style={{ width: "100%", justifyContent: "space-between" }}>
+                    <Row>
+                        <Col xs={24} xl={20} style={{width:'100%'}}>
+                            <div className={GoodStyle.menu}>
+                                <h3>{` ${productData.length} Product(s) found`}</h3>
+                            </div>
+                        </Col>
+                        <Col xs={24} xl={4} style={{width:'100%'}}>
+                            <div className={GoodStyle.menu} style={{ display: "flex", marginRight: '0px', alignItems: 'right' }}>
+                                <h3 style={{ fontWeight: 'bold' }} onChange={this.handleSizeChange}>Order by &nbsp;</h3>
+                                <span >
+                                    <Menu theme="dark" style={{ color: 'white' }} mode="horizontal" defaultSelectedKeys={['1']} >
+                                        <SubMenu icon={<ProjectOutlined />}  title="Ranking">
+                                            <Menu.Item key="setting:1" onClick={() => this.onCollate()}>Ranking</Menu.Item>
+                                            <Menu.Item key="setting:2" onClick={() => this.onCollate('up')}>Lowest to Highest</Menu.Item>
+                                            <Menu.Item key="setting:3" onClick={() => this.onCollate('down')}>Highest to Lowest</Menu.Item>
+                                        </SubMenu>
+                                    </Menu>
+                                </span>
+                            </div>
+                        </Col>
+                    </Row>
                 </div>
 
 
@@ -137,7 +165,7 @@ class Goods extends React.Component {
                         <SyncOutlined spin />
                     </div>) : list}
                 </div>
-                
+
 
 
             </>
--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -42,15 +42,15 @@ class IndexPage extends React.Component {
       <div >
         <Row >
           <Layout style={{ background: 'white' }}>
-            <Col xs={6} xl={4}>
-              <Sider theme="light" style={{ zIndex: 1, width: '100%', marginLeft: 74, marginTop: 84 }}>
+            <Col xs={3} xl={4}>
+              <Sider theme="light" className={styles.sider} >
                 <div className="logo" />
                 <Screen />
               </Sider>
             </Col>
-            <Col xs={18} xl={20}>
+            <Col xs={21} xl={20}>
               <Layout style={{ background: 'white' }}>
-                <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64, background: "white" }}>
+                <Content className={styles.content}>
                   <div className={styles.productlist} style={{ padding: 24, minHeight: 580, display: 'flex', flexFlow: 'row wrap', justifyContent: 'space-around', width: '100%' }}>
                     <Goods />
                   </div>
@@ -58,9 +58,9 @@ class IndexPage extends React.Component {
                 <Footer style={{ textAlign: 'center', fontSize: "30px" }}></Footer>
               </Layout>
             </Col>
-            <div style={{ zIndex: 2, top: 16, left: "80%" }} >
-              <Badge count={count} showZero offset={[-9, 28]} size="small" style={{ backgroundColor: '#1890FF' }}>
-                <Button icon={<ShoppingCartOutlined />} style={{ fontSize: "16px", backgroundColor: 'black', border: 'none' }} type="primary" size="large" shape="" onClick={this.showDrawer} >
+            <div style={{ zIndex: 2, top: 0, right: 0, position:"fixed" }} >
+              <Badge count={count} showZero offset={[-9, 38]} size="large" style={{ backgroundColor: '#1890FF' }}>
+                <Button icon={<ShoppingCartOutlined />} style={{ fontSize: "16px", backgroundColor: 'black', border: 'none', width:'50px', height:'50px' }} type="primary" size="large" shape="" onClick={this.showDrawer} >
 
                 </Button>
               </Badge>
